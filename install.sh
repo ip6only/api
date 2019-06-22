762a45bd53de3c93691c68402c11fbd80aa3f232
Benjamin Howe <ben@bh96.uk>
2019-06-22 17:57:06 +0000

    Added Chrome depenedencies to install script

--- install.sh
+++ install.sh
@@ -37,6 +37,10 @@ printf 'Installing packages... '
 apt install -y git nginx nodejs squid ufw >/dev/null 2>&1
 printf 'Done!\n'
 
+printf 'Installing Chrome dependencies... '
+apt install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget >/dev/null 2>&1
+printf 'Done!\n'
+
 printf 'Configuring firewall... '
 ufw default allow outgoing >/dev/null 2>&1
 ufw default deny incoming >/dev/null 2>&1
