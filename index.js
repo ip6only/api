6a665c8ccac623756236f16ca61395350b19d852
Benjamin Howe <ben@bh96.uk>
2019-07-10 21:43:02 +0000

    Experimental: switched to using built in Chromium

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ 'executablePath': '/usr/bin/chromium-browser' });
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({path: 'example.png'});
--- install.sh
+++ install.sh
@@ -39,11 +39,7 @@ printf 'Done!\n'
 printf 'Installing packages... '
 echo iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean false | debconf-set-selections
-apt install -y git iptables iptables-persistent nginx nodejs squid >/dev/null 2>&1
-printf 'Done!\n'
-
-printf 'Installing Chrome dependencies... '
-apt install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget >/dev/null 2>&1
+apt install -y chromium-browser git iptables iptables-persistent nginx nodejs squid >/dev/null 2>&1
 printf 'Done!\n'
 
 printf 'Configuring firewall... '
