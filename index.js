84c95fc8d37388d4eb8a842671fcdc502fd2dbe1
Benjamin Howe <ben@bh96.uk>
2019-07-10 21:56:39 +0000

    Fixed bug with Raspberry Pi

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 const puppeteer = require('puppeteer-core');
 
 (async () => {
-  const browser = await puppeteer.launch({ 'executablePath': '/usr/bin/chromium-browser' });
+  const browser = await puppeteer.launch({
+    'args': ['--headless', '--disable-gpu', '--disable-features=VizDisplayCompositor'],
+    'executablePath': '/usr/bin/chromium-browser'
+  });
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({path: 'example.png'});
