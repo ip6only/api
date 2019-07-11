84ddf2a6229aed59e14acdeae0310f3e046497f5
Benjamin Howe <ben@bh96.uk>
2019-07-11 19:48:05 +0000

    Prevented firewall bypass by adding a list of whitelisted ports

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const puppeteer = require('puppeteer-core');
 const app = express();
 const port = 3000;
 
+const webPorts = [
+  '80',
+  '443',
+  '591',
+  '2082',
+  '2083',
+  '2086',
+  '2087',
+  '2095',
+  '2096',
+  '8008',
+  '8080',
+  '8443',
+  '8880'
+];
+
 async function generateErrorImage(text) {
     image = await new jimp(800, 600, '#C0C0C0');
     font = await jimp.loadFont(jimp.FONT_SANS_16_BLACK);
@@ -17,26 +33,33 @@ async function generateErrorImage(text) {
 app.get('/favicon.ico', (req, res) => { return res.status(404).send() });
 
 app.get('/screenshot/:url', async (req, res) => {
-  const url = decodeURIComponent(req.params.url);
-  const browser = await puppeteer.launch({
-    'args': ['--headless', '--disable-gpu', '--disable-features=VizDisplayCompositor'],
-    'executablePath': '/usr/bin/chromium-browser'
-  });
-  const page = await browser.newPage();
   let image = undefined;
   let imageContentType = undefined;
-  try {
-    await page.goto(url);
-    image = await page.screenshot({
-      'fullPage': true,
-      'type': 'jpeg'
-    });
-    imageContentType = 'image/jpeg';
-  } catch(exception) {
-    image = await generateErrorImage(exception.message);
+  const url = new URL(decodeURIComponent(req.params.url));
+  if (url.port && !(webPorts.includes(url.port))) {
+    // if we're using a non-default port that hasn't been permitted, block
+    image = await generateErrorImage('Error: port ' + url.port + ' not permitted');
     imageContentType = 'image/png';
   }
-  await browser.close();
+  if (!image) { // if we don't yet have an image, generate one
+    const browser = await puppeteer.launch({
+      'args': ['--headless', '--disable-gpu', '--disable-features=VizDisplayCompositor'],
+      'executablePath': '/usr/bin/chromium-browser'
+    });
+    const page = await browser.newPage();
+    try {
+      await page.goto(url);
+      image = await page.screenshot({
+        'fullPage': true,
+        'type': 'jpeg'
+      });
+      imageContentType = 'image/jpeg';
+    } catch(exception) {
+      image = await generateErrorImage('Error: ' + exception.message);
+      imageContentType = 'image/png';
+    }
+    await browser.close();
+  }
   res.writeHead(200, {
     'Content-Type': imageContentType,
     'Content-Length': image.length
