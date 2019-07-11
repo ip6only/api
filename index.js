e6e2f49f2fe191fa68cac643d79520350503975d
Benjamin Howe <ben@bh96.uk>
2019-07-11 15:29:16 +0000

    Added error message (e.g. where no IPv6 address exists)

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
 
 const express = require('express');
+const jimp = require('jimp');
 const puppeteer = require('puppeteer-core');
 
 const app = express();
 const port = 3000;
+
+const url = 'https://github.com';
+
+async function generateErrorImage(text) {
+    image = await new jimp(800, 600, '#C0C0C0');
+    font = await jimp.loadFont(jimp.FONT_SANS_16_BLACK);
+    image.print(font, 0, 0, { 'text': text, 'alignmentX': jimp.HORIZONTAL_ALIGN_CENTER, 'alignmentY': jimp.VERTICAL_ALIGN_MIDDLE }, 800, 600);
+    return await image.getBufferAsync(jimp.MIME_PNG);
+}
 
 app.get('/', async (req, res) => {
   const browser = await puppeteer.launch({
@@ -12,17 +22,25 @@ app.get('/', async (req, res) => {
     'executablePath': '/usr/bin/chromium-browser'
   });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  const screenshot = await page.screenshot({
-    'fullPage': true,
-    'type': 'jpeg'
-  });
+  let image = undefined;
+  let imageContentType = undefined;
+  try {
+    await page.goto(url);
+    image = await page.screenshot({
+      'fullPage': true,
+      'type': 'jpeg'
+    });
+    imageContentType = 'image/jpeg';
+  } catch(exception) {
+    image = await generateErrorImage(exception.message);
+    imageContentType = 'image/png';
+  }
   await browser.close();
   res.writeHead(200, {
-    'Content-Type': 'image/jpeg',
-    'Content-Length': screenshot.length
+    'Content-Type': imageContentType,
+    'Content-Length': image.length
   });
-  res.end(screenshot, 'binary');
+  res.end(image, 'binary');
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
