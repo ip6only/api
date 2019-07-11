5eabade753041801dafee7408bebfa609c5085c0
Benjamin Howe <ben@bh96.uk>
2019-07-11 23:26:55 +0000

    Cleaned code somewhat

--- index.js
+++ index.js
@@ -33,58 +33,63 @@ const maximumWidth = 1920;
 const defaultHeight = 800;
 const maximumHeight = 1200;
 
-async function generateErrorImage(text, width, height) {
-    image = await new jimp(width, height, '#C0C0C0');
-    font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
-    image.print(font, 0, 0, { 'text': 'Error: ' + text, 'alignmentX': jimp.HORIZONTAL_ALIGN_CENTER, 'alignmentY': jimp.VERTICAL_ALIGN_MIDDLE }, width, height);
-    return await image.getBufferAsync(jimp.MIME_PNG);
-}
+let width = undefined;
+let height = undefined;
 
 app.get('/favicon.ico', (req, res) => { return res.status(404).send() });
 
 app.get('/screenshot/:url', async (req, res) => {
-  // set up image and imageContentType
-  let image = undefined;
-  let imageContentType = undefined;
+  try {
+    // set width
+    width = defaultWidth;
+    if (req.query.width) {
+      width = parseInt(req.query.width);
+    }
+    if (isNaN(width)) {
+      const image = await generateErrorImage('width ' + req.query.width + ' doesn\'t appear to be a number');
+      sendImage(res, image, 'image/png');
+      return;
+    }
+    if (maximumWidth < width) {
+      const image = await generateErrorImage('maximum width ' + maximumWidth + ' exceeded');
+      sendImage(res, image, 'image/png');
+      return;
+    }
 
-  // set width
-  let width = defaultWidth;
-  if (req.query.width && !isNaN(parseInt(req.query.width))) {
-    width = parseInt(req.query.width);
-  }
-  if (maximumWidth < width) {
-    image = await generateErrorImage('maximum width ' + maximumWidth + ' exceeded', defaultWidth, defaultHeight);
-    imageType = 'image/png';
-  }
+    // set height
+    height = defaultHeight;
+    if (req.query.height) {
+      height = parseInt(req.query.height);
+    }
+    if (isNaN(height)) {
+      const image = await generateErrorImage('height ' + req.query.height + ' doesn\'t appear to be a number');
+      sendImage(res, image, 'image/png');
+      return;
+    }
+    if (maximumHeight < height) {
+      const image = await generateErrorImage('maximum height ' + maximumHeight + ' exceeded');
+      sendImage(res, image, 'image/png');
+      return;
+    }
 
-  // set height
-  let height = defaultHeight;
-  if (req.query.height && !isNaN(parseInt(req.query.height))) {
-    height = parseInt(req.query.height);
-  }
-  if (maximumHeight < height) {
-    image = await generateErrorImage('maximum height ' + maximumHeight + ' exceeded', defaultWidth, defaultHeight);
-    imageType = 'image/png';
-  }
+    // set URL
+    const url = new URL(decodeURIComponent(req.params.url));
 
-  // set URL
-  const url = new URL(decodeURIComponent(req.params.url));
+    // check port
+    if (url.port && !permittedPorts.includes(url.port)) {
+      // if we're using a non-default port that hasn't been permitted, block
+      const image = await generateErrorImage('port ' + url.port + ' not permitted', width, height);
+      sendImage(res, image, 'image/png');
+      return;
+    }
 
-  // check port
-  if (url.port && !permittedPorts.includes(url.port)) {
-    // if we're using a non-default port that hasn't been permitted, block
-    image = await generateErrorImage('port ' + url.port + ' not permitted', width, height);
-    imageType = 'image/png';
-  }
+    // check protocol
+    if (!permittedProtocols.includes(url.protocol.slice(0, -1))) {
+      const image = await generateErrorImage('protocol ' + url.protocol.slice(0, -1) + ' not permitted', width, height);
+      sendImage(res, image, 'image/png');
+      return;
+    }
 
-  // check protocol
-  if (!permittedProtocols.includes(url.protocol.slice(0, -1))) {
-    image = await generateErrorImage('protocol ' + url.protocol.slice(0, -1) + ' not permitted', width, height);
-    imageType = 'image/png';
-  }
-
-  // if we don't yet have an image, generate one
-  if (!image) {
     // launch browser
     const browser = await puppeteer.launch({
       'args': ['--headless', '--disable-gpu', '--disable-features=VizDisplayCompositor'],
@@ -93,48 +98,65 @@ app.get('/screenshot/:url', async (req, res) => {
 
     // set up page and viewport
     const page = await browser.newPage();
-    page.setViewport({ 'width': width, 'height': height, 'deviceScaleFactor': 1 });
+    page.setViewport({ width, height });
 
-    try {
-      // attempt to navigate to the URL
-      await page.goto(url);
+    // attempt to navigate to the URL
+    await page.goto(url);
 
-      // set offset (for screenshot)
-      let offset = 0;
-      if (req.query.offset && !isNaN(parseInt(req.query.offset))) {
-        offset = parseInt(req.query.offset);
-      }
-
-      // get document height
-      const documentHeight = await page.evaluate(() => {
-        return document.documentElement.scrollHeight;
-      });
-
-      // check we're not exceeding the document bounds
-      if (documentHeight < (offset + height)) {
-        height = documentHeight - offset;
-      }
-
-      // take screenshot and set content type
-      image = await page.screenshot({
-        'clip': {'x': 0, 'y': offset, 'width': width, 'height': height},
-        'type': 'jpeg'
-      });
-      imageType = 'image/jpeg';
-    } catch(exception) {
-      // generate error and set content type
-      image = await generateErrorImage(exception.message, width, height);
-      imageType = 'image/png';
+    // set offset (for screenshot)
+    let offset = 0;
+    if (req.query.offset && !isNaN(parseInt(req.query.offset))) {
+      offset = parseInt(req.query.offset);
     }
-    await browser.close();
-  }
 
-  // send the response back to the browser
-  res.writeHead(200, {
-    'Content-Type': imageType,
-    'Content-Length': image.length
-  });
-  res.end(image, 'binary');
+    // get document height
+    const documentHeight = await page.evaluate(() => {
+      return document.documentElement.scrollHeight;
+    });
+
+    // check we're not exceeding the document bounds
+    if (documentHeight < (offset + height)) {
+      height = documentHeight - offset;
+    }
+
+    // take screenshot and set content type
+    const image = await page.screenshot({
+      'clip': {'x': 0, 'y': offset, width, height},
+      'type': 'jpeg'
+    });
+    sendImage(res, image, 'image/jpeg');
+  } catch(exception) {
+    // generate error and set content type
+    const image = await generateErrorImage(exception.message, width, height);
+    sendImage(res, image, 'image/png');
+  }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`ip6only API listening on port ${port}!`));
+
+async function generateErrorImage(text, width=defaultWidth, height=defaultHeight) {
+    const image = await new jimp(width, height, '#C0C0C0');
+    const font = await selectFontSizeFromWidth(width);
+    text = 'Error: ' + text;
+    const alignmentX = jimp.HORIZONTAL_ALIGN_CENTER;
+    const alignmentY = jimp.VERTICAL_ALIGN_MIDDLE;
+    image.print(font, 0, 0, { text, alignmentX, alignmentY }, width, height);
+    return await image.getBufferAsync('image/png');
+}
+
+async function selectFontSizeFromWidth(width) {
+  let font = undefined;
+  if (width < 800) {
+    font = await jimp.loadFont(jimp.FONT_SANS_16_BLACK);
+  } else if (width < 1400) {
+    font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+  } else {
+    font = await jimp.loadFont(jimp.FONT_SANS_64_BLACK);
+  }
+  return font;
+}
+
+function sendImage(response, image, imageType) {
+  response.type(imageType);
+  response.send(image);
+}
